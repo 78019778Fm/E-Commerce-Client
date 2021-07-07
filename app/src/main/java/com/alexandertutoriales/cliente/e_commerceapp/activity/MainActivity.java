042e883bf7558960aeb587a172bc12f249d6275b
Alexander Fuentes Medina <alexanderfuentes199912@gmail.com>
2021-07-07 12:15:09 -0500

    Toast Personalizado y Validar TextInputLayout and EditText

--- app/src/main/java/com/alexandertutoriales/cliente/e_commerceapp/activity/MainActivity.java
+++ app/src/main/java/com/alexandertutoriales/cliente/e_commerceapp/activity/MainActivity.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,6 +23,7 @@ import com.alexandertutoriales.cliente.e_commerceapp.entity.service.Usuario;
 import com.alexandertutoriales.cliente.e_commerceapp.utils.DateSerializer;
 import com.alexandertutoriales.cliente.e_commerceapp.utils.TimeSerializer;
 import com.alexandertutoriales.cliente.e_commerceapp.viewmodel.UsuarioViewModel;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -30,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText edtMail, edtPassword;
     private Button btnIniciarSesion;
     private UsuarioViewModel viewModel;
-    private TextView txtInputUsuario, txtInputPassword;
+    private TextInputLayout txtInputUsuario, txtInputPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,31 +49,122 @@ public class MainActivity extends AppCompatActivity {
     private void initViewModel() {
         viewModel = new ViewModelProvider(this).get(UsuarioViewModel.class);
     }
-    private void init(){
+
+    private void init() {
         edtMail = findViewById(R.id.edtMail);
         edtPassword = findViewById(R.id.edtPassword);
+        txtInputUsuario = findViewById(R.id.txtInputUsuario);
+        txtInputPassword = findViewById(R.id.txtInputPassword);
         btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
         btnIniciarSesion.setOnClickListener(v -> {
-            viewModel.login(edtMail.getText().toString(), edtPassword.getText().toString()).observe(this, response ->{
-                if(response.getRpta() == 1){
-                    Toast.makeText(this, response.getMessage(), Toast.LENGTH_SHORT).show();
-                    Usuario u = response.getBody();
-                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-                    SharedPreferences.Editor editor = preferences.edit();
-                    final Gson g = new GsonBuilder()
-                            .registerTypeAdapter(Date.class, new DateSerializer())
-                            .registerTypeAdapter(Time.class, new TimeSerializer())
-                            .create();
-                    editor.putString("UsuarioJson", g.toJson(u, new TypeToken<Usuario>(){
-                    }.getType()));
-                    editor.apply();
-                    edtMail.setText("");
-                    edtPassword.setText("");
-                    startActivity(new Intent(this, InicioActivity.class));
-                }else{
-                    Toast.makeText(this, "Ocurrio un error " + response.getMessage() , Toast.LENGTH_SHORT).show();
+            try {
+                if (validar()) {
+                    viewModel.login(edtMail.getText().toString(), edtPassword.getText().toString()).observe(this, response -> {
+                        if (response.getRpta() == 1) {
+                            //Toast.makeText(this, response.getMessage(), Toast.LENGTH_SHORT).show();
+                            toastCorrecto(response.getMessage());
+                            Usuario u = response.getBody();
+                            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                            SharedPreferences.Editor editor = preferences.edit();
+                            final Gson g = new GsonBuilder()
+                                    .registerTypeAdapter(Date.class, new DateSerializer())
+                                    .registerTypeAdapter(Time.class, new TimeSerializer())
+                                    .create();
+                            editor.putString("UsuarioJson", g.toJson(u, new TypeToken<Usuario>() {
+                            }.getType()));
+                            editor.apply();
+                            edtMail.setText("");
+                            edtPassword.setText("");
+                            startActivity(new Intent(this, InicioActivity.class));
+                        } else {
+                            toastIncorrecto("Credenciales Inválidas");
+                        }
+                    });
+                } else {
+                    toastIncorrecto("Por favor, complete todos los campos.");
                 }
-            });
+            } catch (Exception e) {
+                toastIncorrecto("Se ha producido un error al intentar loguearte : " + e.getMessage());
+            }
         });
+        edtMail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                txtInputUsuario.setErrorEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        edtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                txtInputPassword.setErrorEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
+    public void toastCorrecto(String msg) {
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.custom_toast_ok, (ViewGroup) findViewById(R.id.ll_custom_toast_ok));
+        TextView txtMensaje = view.findViewById(R.id.txtMensajeToast1);
+        txtMensaje.setText(msg);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.BOTTOM, 0, 200);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(view);
+        toast.show();
+    }
+
+    public void toastIncorrecto(String msg) {
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.custom_toast_error, (ViewGroup) findViewById(R.id.ll_custom_toast_error));
+        TextView txtMensaje = view.findViewById(R.id.txtMensajeToast2);
+        txtMensaje.setText(msg);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.BOTTOM, 0, 200);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(view);
+        toast.show();
+    }
+
+    private boolean validar() {
+        boolean retorno = true;
+        String usuario, password;
+        usuario = edtMail.getText().toString();
+        password = edtPassword.getText().toString();
+        if (usuario.isEmpty()) {
+            txtInputUsuario.setError("Ingrese su usario y/o correo electrónico");
+            retorno = false;
+        } else {
+            txtInputUsuario.setErrorEnabled(false);
+        }
+        if (password.isEmpty()) {
+            txtInputPassword.setError("Ingrese su contraseña");
+            retorno = false;
+        } else {
+            txtInputPassword.setErrorEnabled(false);
+        }
+        return retorno;
     }
 }
